3fffbb7b76c787b4ad464dc7d5c1fa631b3d3898
arshia11d <arshia11d@outlook.com>
2021-11-21 21:19:27 +0100

    feat: lit-slider and lit-slide first implementation

---
+++ demo/Demo.ts
@@ -0,0 +1,16 @@
+import { LitElement, html } from 'lit'
+import { customElement } from 'lit/decorators.js'
+
+@customElement('lit-slider-demo')
+export class Demo extends LitElement {
+	protected render() {
+		return html`
+			<div>
+				<lit-slider style='height: 500px'>
+					<lit-slide style='background: red'>Slide 1</lit-slide>
+					<lit-slide style='background: yellow'>Slide 2</lit-slide>
+				</lit-slider>
+			</div>
+		`
+	}
+}
